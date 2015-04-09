17e80529dd94c0856945acb8c2c6d3eb16ab4508
Liuchenling <wssgcg1213@qq.com>
2015-04-09 22:07:31 +0800

    clear console and module fix

--- src/lib/counter.js
+++ src/lib/counter.js
@@ -1,7 +1,6 @@
 /*! Ling 09-04-2015 */
 define(['jQuery'], function($) {
-    console.log('this is module counter!');
-    console.log($);
+    console.log('counter module loaded!');
     $.fn.counter = function(opts) {
         var innerHTML = '<span class="decor top"></span><span class="decor bottom"></span><span class="from top"><span></span><span class="shadow"></span></span><span class="from bottom"><span></span><span class="shadow"></span></span><span class="to top"><span></span><span class="shadow"></span></span><span class="to bottom"><span></span><span class="shadow"></span></span>';
         var self = $(this);
@@ -67,57 +66,6 @@ define(['jQuery'], function($) {
             }
         }
     };
-
-
-    //$(function(){
-    //    //#day, #hour, #minute,
-    //    $('#sec').each(function(i, node){
-    //        $(node).counter();
-    //    });
-    //});
-
-
-    var END_DATE = '2015-05-16 10:00:00',
-        timestamp_end = parseInt(new Date(END_DATE).getTime()) / 1000,
-        now = parseInt(new Date().getTime() / 1000),
-        time_intval = timestamp_end - now;
-    var day, hour, minute, second;
-    if(time_intval <= 0){
-        day = hour = minute = second = 0;
-    }else{
-        day = parseInt(time_intval / 86400);
-        hour = parseInt((time_intval - day * 86400 ) / 3600);
-        minute = parseInt((time_intval - day * 86400 - hour * 3600) / 60);
-        second = parseInt(time_intval - day * 86400 - hour * 3600 - minute * 60);
-        //console.log(day, hour, minute, second);
-        $('#sec').counter({
-            counter: second,
-            intval: 1, //单位是秒
-            loop: true,
-            top: 60,
-            firstIntval: 1
-        });
-        $('#minute').counter({
-            counter: minute,
-            intval: 60,
-            loop: true,
-            top: 60,
-            firstIntval: second
-        });
-        $('#hour').counter({
-            counter: hour,
-            intval: 60 * 60,
-            loop: true,
-            top: 24,
-            firstIntval: minute * 60 + second
-        });
-        $('#day').counter({
-            counter: day,
-            intval: 60 * 60 * 24,
-            loop: false,
-            firstIntval: hour * 3600 + minute * 60 + second
-        });
-    }
 
 
 });
--- src/lib/main.js
+++ src/lib/main.js
@@ -14,5 +14,47 @@ require.config({
 });
 
 require(['counter'], function() {
+    var END_DATE = '2015-05-16 10:00:00',
+        timestamp_end = parseInt(new Date(END_DATE).getTime()) / 1000,
+        now = parseInt(new Date().getTime() / 1000),
+        time_intval = timestamp_end - now;
+    var day, hour, minute, second;
+    if(time_intval <= 0){
+        day = hour = minute = second = 0;
+    }else{
+        day = parseInt(time_intval / 86400);
+        hour = parseInt((time_intval - day * 86400 ) / 3600);
+        minute = parseInt((time_intval - day * 86400 - hour * 3600) / 60);
+        second = parseInt(time_intval - day * 86400 - hour * 3600 - minute * 60);
+        //console.log(day, hour, minute, second);
+        $('#sec').counter({
+            counter: second,
+            intval: 1, //单位是秒
+            loop: true,
+            top: 60,
+            firstIntval: 1
+        });
+        $('#minute').counter({
+            counter: minute,
+            intval: 60,
+            loop: true,
+            top: 60,
+            firstIntval: second
+        });
+        $('#hour').counter({
+            counter: hour,
+            intval: 60 * 60,
+            loop: true,
+            top: 24,
+            firstIntval: minute * 60 + second
+        });
+        $('#day').counter({
+            counter: day,
+            intval: 60 * 60 * 24,
+            loop: false,
+            firstIntval: hour * 3600 + minute * 60 + second
+        });
+    }
+
     console.log('all modules loaded!');
 });
